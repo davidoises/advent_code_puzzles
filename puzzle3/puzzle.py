ea1b71880567c0ddfffe6ebb46dd131adedffba5
davidoises <davidoises@hotmail.com>
2025-07-27 13:40:12 +0200

    Part 2 finished

--- puzzle3/puzzle.py
+++ puzzle3/puzzle.py
@@ -30,3 +30,32 @@ if __name__ == "__main__":
         # print(f"Found: mul({x},{y}) = {res}")
     print("total res: ", total_res)
 
+    # Part 2
+
+    # Apply a regular expression to get the expected format mul(x,y), do(), and don't().
+    matches = re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|do\(\)|don\'t\(\)", memory_contents)
+
+    # Iterate through instructions and keep track if enabled or not
+    enable_mul = True
+    total_res = 0
+    for instruction in matches:
+        # Check if enable or disable the instructions
+        if instruction == "do()":
+            enable_mul = True
+        elif instruction == "don't()":
+            enable_mul = False
+        else:
+            if enable_mul:
+                # If enabled, extract the multiplication arguments
+                inner_match = re.match(r"mul\((\d{1,3}),(\d{1,3})\)", instruction)
+                x = int(inner_match.group(1))
+                y = int(inner_match.group(2))
+
+                # Perform the multiplication
+                res = x*y
+                total_res += res
+                
+    print("total res: ", total_res)
+
+
+
